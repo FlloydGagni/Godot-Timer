616919af68c71d89c5494462c9b0d2f9b13dea5a
Flloyd Nathaniel Gagni <fnbg720@gmail.com>
2025-06-02 15:03:15 +0800

    Finish timer
    Signed-off-by: Flloyd Nathaniel Gagni <fnbg720@gmail.com>

---
+++ scenes/main.gd
@@ -0,0 +1,58 @@
+extends Control
+
+var time_left : int = 0
+var session_time_start_value : int = 0
+var is_running : bool = false
+
+@onready var timer_label = $MarginContainer/VBoxContainer/VBoxTimerDisplayContainer/TimerDisplay
+@onready var minutes_input = $MarginContainer/VBoxContainer/VBoxTimerDisplayContainer/HBoxContainer/MinutesValue
+@onready var seconds_input = $MarginContainer/VBoxContainer/VBoxTimerDisplayContainer/HBoxContainer2/SecondsValue
+@onready var timer_node = $Timer
+
+func _ready() -> void:
+	update_timer_display()
+	$MarginContainer/VBoxContainer/VBoxButtonContainer/START.pressed.connect(start_timer)
+	$MarginContainer/VBoxContainer/VBoxButtonContainer/PAUSE.pressed.connect(pause_timer)
+	$MarginContainer/VBoxContainer/VBoxButtonContainer/RESET.pressed.connect(reset_timer)
+	$HBoxContainer/EXIT.pressed.connect(exit_timer)
+	$MarginContainer/VBoxContainer/VBoxTimerDisplayContainer/SET_TIMER.pressed.connect(set_timer)
+	timer_node.timeout.connect(on_timer_tick)
+
+func update_timer_display() -> void:
+	var minutes = time_left / 60
+	var seconds = time_left % 60
+	
+	timer_label.text = "%02d:%02d" % [minutes, seconds]
+
+func start_timer() -> void:
+	if not is_running:
+		is_running = true
+		timer_node.start()
+
+func pause_timer() -> void:
+	if is_running:
+		is_running = false
+		timer_node.stop()
+
+func reset_timer() -> void:
+	is_running = false
+	timer_node.stop()
+	time_left = session_time_start_value
+	update_timer_display()
+
+func on_timer_tick() -> void:
+	time_left -= 1
+	update_timer_display()
+
+func exit_timer() -> void:
+	print("Exit Button Pressed")
+	get_tree().quit()
+
+func set_timer() -> void:
+	var minutes = minutes_input.value
+	var seconds = seconds_input.value
+	
+	time_left = int(minutes) * 60 + int(seconds)
+	session_time_start_value = time_left
+	
+	update_timer_display()
